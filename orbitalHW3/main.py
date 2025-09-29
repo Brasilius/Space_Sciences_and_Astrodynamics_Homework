ced6a32b6144f6e5c915ff73150c78f79e15ea75
Brasilius <niels.l.larsen-1@ou.edu>
2025-09-28 20:34:25 -0500

    add problem 4

--- orbitalHW3/main.py
+++ orbitalHW3/main.py
@@ -42,7 +42,24 @@ def main():
     print("The orbital speed at apogee is: ", orbital_speed_apogee, "m/s")
     print("The orbital period is: ", orbital_period_3, "s")
     #conclude problem 3
-
+    #start problem 4
+    print("An earth satellite has a speed of 7.5 km/s and a flight path angle of 10 degrees when its radius is 8000 km")
+    satellite_speed = 7.5 #noted as km/s
+    flight_path_angle = 10 #Noted as degrees
+    satellite_radius = 8000 #noted as km
+    satellite_speed = satellite_speed * 1000 #transform into m/s
+    satellite_radius = satellite_radius * 1000 #transform into m
+    flight_path_angle = math.radians(flight_path_angle) #transform into radians
+    semi_major_axis_4 = 1/((2/satellite_radius)-((satellite_speed**2)/(earth_gravity*earth_mass)))
+    eccentricity_4 = math.sqrt(1 + ((satellite_radius*satellite_speed*math.cos(flight_path_angle))**2/(earth_gravity*earth_mass*semi_major_axis_4 -1)))
+    perigee_4 = semi_major_axis_4*(1-eccentricity_4)
+    apogee_4 = semi_major_axis_4*(1+eccentricity_4)
+    print("The semi-major axis is: ", semi_major_axis_4/1000, "km")
+    print("The eccentricity is: ", eccentricity_4)
+    print("The perigee is: ", perigee_4/1000, "km")
+    print("The apogee is: ", apogee_4/1000, "km")
+    #conclude problem 4
+    
 
 
 
